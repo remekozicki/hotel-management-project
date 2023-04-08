df1c5532d069199b68000d5b62320bf188a6ced8
remekozicki <remigiusz.kozicki@gmail.com>
2023-04-08 15:27:54 +0200

    roomlist

--- reactjs-hotel-management/src/App.tsx
+++ reactjs-hotel-management/src/App.tsx
@@ -1,8 +1,12 @@
-import Message from "./componets/Message";
+import Message from "./components/Message";
+import RoomList from "./components/RoomList";
 
 function App(){
+
+
     return <div>
         <Message></Message>
+        <RoomList></RoomList>
     </div>
 }
 
