6b1065fca66b0ca77832c866ee9978520598fa0f
wiktorwozny <wikivip10@gmail.com>
2023-05-01 17:39:51 +0200

    getting started with front

--- reactjs-hotel-management/src/App.tsx
+++ reactjs-hotel-management/src/App.tsx
@@ -1,37 +1,9 @@
-import Message from "./components/Message";
-import RoomList from "./components/RoomList";
-import Alert from "./components/Alert";
-import ButtonTut from "./components/ButtonTut";
 import {useState} from "react";
 
-function App(){
-    const [alertVisible, setAlertVisibility] = useState(false)
-
-    const rooms = [
-        'pokoj 1',
-        'pokoj 2',
-        'pokoj 3',
-        'pokoj 4',
-        'pokoj 5',
-    ];
-
-    const handleSelectItem = (item: string) => {
-        console.log(item);
-    }
+function App() {
 
     return <div>
-        <RoomList
-            items = {rooms}
-            heading="Cities"
-            onSelectItem={handleSelectItem}/>
-        {alertVisible && <Alert onClose={() => setAlertVisibility(false)}> Hello world</Alert>}
 
-        <ButtonTut
-            color='danger'
-            onClick={() => setAlertVisibility(true)}
-        >
-            My button
-        </ButtonTut>
     </div>
 }
 
--- reactjs-hotel-management/src/components/Message.tsx
+++
@@ -1,9 +0,0 @@
-
-function Message(){
-    const name = 'wiktor to leń';
-    if(name){
-        return <h1> hello {name}</h1>
-    }
-    return <h1>hello world!</h1>;
-}
-export default Message
