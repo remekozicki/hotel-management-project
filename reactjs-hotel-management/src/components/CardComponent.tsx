f3ce3d753c8608ca563a6e89c142dd6986e5eeed
wiktorwozny <wikivip10@gmail.com>
2023-05-01 19:45:13 +0200

    routing added

--- reactjs-hotel-management/src/components/CardComponent.tsx
+++ reactjs-hotel-management/src/components/CardComponent.tsx
@@ -1,5 +1,26 @@
-function CardComponent(): JSX.Element {
-    return <div></div>
+import RoomType from "../models/RoomType";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import React from "react";
+
+function CardComponent(roomType: RoomType): JSX.Element {
+    return (
+        <Card style={{width: '18rem'}}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap"/>
+            <Card.Body>
+                <Card.Title>{roomType.type}</Card.Title>
+                <Card.Text>
+                    {roomType.description}
+                </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroup.Item>size: {roomType.size}</ListGroup.Item>
+                <ListGroup.Item>price: {roomType.price}$</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+                <Card.Link href="#">Show room details</Card.Link>
+            </Card.Body>
+        </Card>)
 }
 
 export default CardComponent;
--- reactjs-hotel-management/src/components/RoomsComponent.tsx
+++ reactjs-hotel-management/src/components/RoomsComponent.tsx
@@ -3,6 +3,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import React, {useState, useEffect} from 'react';
 import data from '../assets/RoomsTypesData.json';
 import RoomType from "../models/RoomType";
+import CardComponent from "./CardComponent";
 
 function RoomsComponent(): JSX.Element {
 
@@ -15,24 +16,10 @@ function RoomsComponent(): JSX.Element {
         <div className='wrapper'>
             <h2 className='p-3'>Available rooms:</h2>
             <div className='d-flex flex-wrap gap-5 justify-content-center'>
-                {rooms.map((room: RoomType) => {
-                    return (
-                        <Card style={{width: '18rem'}}>
-                            <Card.Img variant="top" src="holder.js/100px180?text=Image cap"/>
-                            <Card.Body>
-                                <Card.Title>{room.type}</Card.Title>
-                                <Card.Text>
-                                    {room.description}
-                                </Card.Text>
-                            </Card.Body>
-                            <ListGroup className="list-group-flush">
-                                <ListGroup.Item>size: {room.size}</ListGroup.Item>
-                                <ListGroup.Item>price: {room.price}$</ListGroup.Item>
-                            </ListGroup>
-                            <Card.Body>
-                                <Card.Link href="#">Show room details</Card.Link>
-                            </Card.Body>
-                        </Card>)
+                {rooms.map((roomType: RoomType) => {
+                    return <CardComponent _id={roomType._id} type={roomType.type} image={roomType.image}
+                                          size={roomType.size} price={roomType.price}
+                                          description={roomType.description}></CardComponent>
                 })}
             </div>
         </div>
